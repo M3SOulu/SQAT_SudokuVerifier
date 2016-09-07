13a1cd437c1b6ca1d1979b33fd340984246f244a
jkananen <jkananen@STUPID-PC413.stnet.local>
2016-09-07 16:56:58 +0300

    Refactoring code and tests

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,7 +2,7 @@
 public class SudokuVerifier {
 	
 	public static int verify(String candidateSolution) {
-		if (candidateSolution.length() < 81){
+		if (candidateSolution.length() < 81 || candidateSolution.length() > 81) {
 			return 1;
 		}
 		
