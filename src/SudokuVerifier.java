b0eff6f5c997ed256e2029fe0f9fffe158d742f3
jkananen <jkananen@STUPID-PC413.stnet.local>
2016-09-07 16:52:54 +0300

    Added two tests for the lenght of the string

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,8 +1,11 @@
 
 public class SudokuVerifier {
 	
-	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
+	public static int verify(String candidateSolution) {
+		if (candidateSolution.length() < 81){
+			return 1;
+		}
+		
 		return 0;
 		
 		//check
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,17 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerify_TooShortString() {
+		String testString = "123";
+		SudokuVerifier.verify(testString);
+		assertEquals("Not yet implemented", 1,SudokuVerifier.verify(testString));
+	}
+	
+	@Test
+	public void testVerify_StringLengthIsEnough() {
+		String testString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		SudokuVerifier.verify(testString);
+		assertEquals("Not yet implemented", 0,SudokuVerifier.verify(testString));
 	}
 
 }
