b6113927da9c410219b6d5beeac4f6ba14def53c
Matias Kinnunen <matias.kinnunen@gmail.com>
2016-09-07 16:34:27 +0300

    Add number verification (Rule 1) and first tests

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,10 +1,24 @@
+import java.util.regex.Pattern;
 
 public class SudokuVerifier {
-	
+	/**
+	 * Verifies a Sudoku solution.
+	 * 
+	 * @param candidateSolution All 81 digits
+	 * @return 0 if solution is correct, 1 if incorrect
+	 */
 	public int verify(String candidateSolution) {
-		// returns 0 if the candidate solution is correct
-		return 0;
+		// R1: A cell can only store positive digits, i.e. 1...9
+		Pattern illegalCharsPattern = Pattern.compile("[^1-9]");
+		boolean illegalCharsFound = illegalCharsPattern.matcher(candidateSolution).find();
+		if (illegalCharsFound) {
+			return -1;
+		}
 		
-		//check
+		// R2: All digits appear only once in a sub-grid, i.e. they cannot repeat
+		
+		
+		// Return 0 if the candidate solution is correct
+		return 0;
 	}
 }
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,54 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	public void testVerify_CorrectSolution() {
+		// Arrange
+		String solution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		SudokuVerifier SudokuVerifier = new SudokuVerifier();
+		
+		// Act
+		int verification = SudokuVerifier.verify(solution);
+		
+		// Assert
+		assertEquals("Correct solution", 0, verification);
 	}
 
+	@Test
+	public void testVerify_IncorrectSolution() {
+		// Arrange
+		String solution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		SudokuVerifier SudokuVerifier = new SudokuVerifier();
+		
+		// Act
+		int verification = SudokuVerifier.verify(solution);
+		
+		// Assert
+		assertEquals("Correct solution", 1, verification);
+	}
+	
+	@Test
+	public void testVerify_IncorrectNumbers() {
+		// Arrange
+		String solution = "003456789912345678891234567789123456678912345567891234456789123345678912234567890";
+		SudokuVerifier SudokuVerifier = new SudokuVerifier();
+		
+		// Act
+		int verification = SudokuVerifier.verify(solution);
+		
+		// Assert
+		assertEquals("Correct solution", -1, verification);
+	}
+	
+	@Test
+	public void testVerify_IncorrectCharacters() {
+		// Arrange
+		String solution = "1234567899123456788912345677891234566789123455678912344567891233456789122345abCD-";
+		SudokuVerifier SudokuVerifier = new SudokuVerifier();
+		
+		// Act
+		int verification = SudokuVerifier.verify(solution);
+		
+		// Assert
+		assertEquals("Correct solution", -1, verification);
+	}
 }
