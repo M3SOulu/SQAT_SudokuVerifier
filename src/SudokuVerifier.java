b26d1bd1f8271756047d3948aeb50715cc12025e
jmouruja <jmouruja@STUPID-PC421.stnet.local>
2016-09-07 16:56:39 +0300

    started

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
 
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		ArrayList<E> arraylist = new ArrayList();
 		// returns 0 if the candidate solution is correct
+		for (int i = 0; i <= 81; i++){
+
+
+			String sudokuline = candidateSolution.substring((i - 9), i);
+			arraylist.add()
+			
+		}
 		return 0;
 		
 		//check
