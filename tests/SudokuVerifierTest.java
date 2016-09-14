3587465c0c0ee419637f12346327840f310a71a5
somename <someemail>
2016-09-14 15:24:10 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -15,9 +15,9 @@ public class SudokuVerifierTest {
 		uals(verifier.verify(candidate), 1);
 	}
 	@Test
-	public void testCandidateContains_Zero_1() throws Exception {
+	public void testCandidateContains_InvalidCharacter_1() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
-		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		String candidate = "417369825632158947958724316825a37169791586432346912758289643571573291684164875293";
 		assertEquals(verifier.verify(candidate), 1);
 	}
 	
