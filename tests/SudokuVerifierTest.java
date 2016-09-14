4e6949d8c6bc4d89ee2a8e2a7d69d6be9d39d0a9
somename <someemail>
2016-09-14 15:25:45 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -11,7 +11,7 @@ public class SudokuVerifierTest {
 	public void testCandidateContains_Zero_1() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
 		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(verifier.verify(candidate), 1);
+		assertEquals(verifier.verify(candidate), 2);
 	}
 	
 	@Test
