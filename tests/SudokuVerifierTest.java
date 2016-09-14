0f577dbf81b7d65151e16ef7f3295b691e6421c3
somename <someemail>
2016-09-14 15:22:47 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,8 +8,8 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 	
 	@Test
-	public void testCandidateContains_Zero() 
-	{
+	public void testCandidateContains_Zero() {
+		SudokuVerifier verifier = new SudokuVerifier;
 		
 	}
 	
