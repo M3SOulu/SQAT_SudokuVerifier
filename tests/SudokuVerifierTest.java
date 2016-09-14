c7b0a7c675f91fa39b753bf4a542a8b92264b3ac
somename <someemail>
2016-09-14 15:24:24 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -12,8 +12,14 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
-	public void testPositiveNumbersOrCointainsALetter() {
+	public void testHasNegativeNumbersOrCointainsALetter() {
 		String negativeError = "41736-1825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals(-1, SudokuVerifier.verify(negativeError));
+	}
+	
+	@Test
+	public void testPositiveNumbersAndNoLetters() {
+		String negativeError = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertEquals(-1, SudokuVerifier.verify(negativeError));
 	}
 
