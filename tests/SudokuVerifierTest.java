f7dc895549f77ab7dab8970315fd7fa87a2219b1
somename <someemail>
2016-09-14 15:23:45 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -25,7 +25,7 @@ public class SudokuVerifierTest {
 	public void testVerify_StringIsTooLong() {
 		String testString = "41736982563215894795872431682543716979158643234691275828964312313123123312571573291684164875293";
 		SudokuVerifier.verify(testString);
-		assertEquals("String is too long", 1,SudokuVerifier.verify(testString));
+		assertEquals("String is too long", 0,SudokuVerifier.verify(testString));
 	}
 	
 	@Test
