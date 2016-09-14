b4f74144099b35bb7708af4e295dbbaef5a59389
somename <someemail>
2016-09-14 16:13:57 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -31,8 +31,8 @@ public class SudokuVerifierTest {
 	@Test
 	public void testVerify_splitIntoRowsOfNine() {
 		String testString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		SudokuVerifier.splitStringToRows(testString);
-		assertEquals("There are not 9 rows", 0, SudokuVerifier.splitStringToRows(testString));
+		SudokuVerifier.amountOfRows(testString);
+		assertEquals("There are not 9 rows", 0, SudokuVerifier.amountOfRows(testString));
 	}
 
 }
