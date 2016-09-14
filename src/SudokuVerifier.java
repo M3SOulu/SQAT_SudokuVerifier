84a923df1bf58801b07db6716a6e10d850c172fe
somename <someemail>
2016-09-14 15:25:45 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
 public class SudokuVerifier {
+	private final int GLOBAL_GRID_SIZE = 81;
+	private final int SUB_GRID_SIZE = 9;
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
@@ -15,6 +17,21 @@ public class SudokuVerifier {
 	}
 	
 	private int checkSubGrid(String candidateSolution){
+		int error = 0;
+		int[] testSet = new int[SUB_GRID_SIZE];
 		
+		for (int i = 0; i < GLOBAL_GRID_SIZE; i = i + 9) {
+			// next 9 numbers to test set
+			for (int k = 0; k < SUB_GRID_SIZE; k++) {
+				testSet[k] = Integer.parseInt(candidateSolution[i + k]);
+			}
+			
+			Arrays.sort(testSet);
+		}
+		
+		
+		
+		
+		return error;
 	}
 }
