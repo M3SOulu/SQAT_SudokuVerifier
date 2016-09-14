d7530dbfaa490632b92ebfa15cdcf98a35a39ca7
somename <someemail>
2016-09-14 15:24:46 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -13,8 +13,9 @@ public class SudokuVerifierTest {
 		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertEquals(verifier.verify(candidate), 1);
 	}
+	
 	@Test
-	public void testCandidateContains_Zero_1() throws Exception {
+	public void testCandidateValid() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
 		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertEquals(verifier.verify(candidate), 1);
