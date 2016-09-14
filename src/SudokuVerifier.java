17f6bc466649a7a89f5c2c35ccc8498ca42783c6
somename <someemail>
2016-09-14 16:05:14 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -24,8 +24,8 @@ public class SudokuVerifier {
 	}
 	
 	private int checkSubGrid(String candidateSolution){
-		int error = 0;
 		boolean debug = false;
+		int error = 0;
 		int[] testSet = new int[SUB_GRID_SIZE];
 		
 		for (int i = 0; i < GLOBAL_GRID_SIZE; i = i + 9) {
