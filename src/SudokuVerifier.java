7e3acc30f4f3b3cf7c125c7e7840d1382186c6c9
somename <someemail>
2016-09-14 15:50:00 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -20,7 +20,6 @@ public class SudokuVerifier {
 	public static int splitStringToRows(String candidateSolution){
 		String[] rows = candidateSolution.split("(?<=\\G.{9})");
 		System.out.print(rows[1]);
-		System.out.println(Arrays.toString(rows));
 		//if (isRowLengthRight(row) == 0)
 			//return 0;
 		return -1;
