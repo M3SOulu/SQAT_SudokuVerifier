a48570a66e5a93b4d90e01d644a6dfdbbbd8bd65
somename <someemail>
2016-09-14 16:32:13 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -21,14 +21,14 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
-	public void rule2CorrectForCorrectSudoku() {
+	public void rule3CorrectForCorrectSudoku() {
 		String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertEquals("Failed when checking global row for correct sudoku",
 				verifier.NO_ERROR, verifier.verifyGlobalRow(correctSudoku));
 	}
 	
 	@Test
-	public void rule2CatchesError() {
+	public void rule3CatchesError() {
 		String incorrectSudoku = "123456789912345678891234567789123456678912345567891234456782123345678912234567891";
 		assertEquals("Global row error was not found when it exists",
 				verifier.SUBGRID_ERROR, verifier.verifyGlobalRow(incorrectSudoku));
