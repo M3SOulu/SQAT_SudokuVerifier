1a4ccb962f902386e024c24d991b267c89c372b4
somename <someemail>
2016-09-14 16:16:36 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -25,6 +25,22 @@ public class SudokuVerifierTest {
 	}
 
 	@Test
+	public void testCandidateContains_Zero_Err1() throws Exception {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidate = 
+				  "417369825"
+				+ "632150947" // Zero
+				+ "958724316"
+				+ "825437169"
+				+ "791586432"
+				+ "346912758"
+				+ "289643571"
+				+ "573291684"
+				+ "164875293";;
+		assertEquals(-1, verifier.verify(candidate));
+	}
+
+	@Test
 	public void testCandidateContains_InvalidCharacter_Err1() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
 		String candidate = 
