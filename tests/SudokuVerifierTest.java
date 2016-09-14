3e26518a9baea0ce2378685d987597c14a4e2394
somename <someemail>
2016-09-14 15:24:35 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -20,7 +20,7 @@ public class SudokuVerifierTest {
 	@Test
 	public void testPositiveNumbersAndNoLetters() {
 		String negativeError = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(-1, SudokuVerifier.verify(negativeError));
+		assertEquals(0, SudokuVerifier.verify(negativeError));
 	}
 
 }
