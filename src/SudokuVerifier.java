27c7ad0c84e4493554cb1bd540018765236ac44f
somename <someemail>
2016-09-14 16:41:29 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -88,8 +88,19 @@ public class SudokuVerifier {
 		int[] subgridCellValues = new int[SUBGRID_AND_ROW_SIZE]; 
 		// 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 		
-		final int SUBGRID_CELL_AMOUNT = 3;
-		for (int i = 0; i < SUBGRID_ROW_AMOUNT; k++) {
+		final int SUBGRID_ROW_AMOUNT = 3;
+		for (int i = 0; i < SUBGRID_ROW_AMOUNT; i++) {
+			int k = 0;
+			cellIndex = (i * 9) + (3 * subgridNum);
+			
+			while (k < 3) {
+				
+				cellIndex++;
+				k++;
+			}
+			for (; k < ; k++) {
+				
+			}
 			//use subgrid num in right way instead of int
 			//testSet[k] = Integer.parseInt(Character.toString(candidateSolution.charAt(i + k)));
 		}
