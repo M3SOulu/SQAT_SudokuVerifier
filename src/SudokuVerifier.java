6ab8918fa2bde44213d3ccb39c9c6d07d33e76ae
somename <someemail>
2016-09-14 15:03:50 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,6 +3,10 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
+		
+		if (candidateSolution.length() != 81)
+			throw new Exception("Invalid board dimensions");
+		
 		return 0;
 		
 		//check
