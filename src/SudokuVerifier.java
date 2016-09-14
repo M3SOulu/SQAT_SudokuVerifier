5e04bc9fdf631495c5b9dfb10045c83cdca29fea
somename <someemail>
2016-09-14 15:31:43 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,18 +1,25 @@
 
 public class SudokuVerifier {
 	
-	public int verify(String candidateSolution) {
-		
-		if(candidateSolution.length() != 81) {
-			return -2;
-		}
-		
+	public int verifyCorrectCharacters(String input) {
 		int lengthOf = candidateSolution.length();
 		String legals = "123456789";
 		for (int i = 0; i < lengthOf; i++) {
 			if(legals.indexOf(candidateSolution.charAt(i)) < 0) {
 				return -1;
 			}
+		}
+		return 0;
+	}
+	
+	public int verify(String candidateSolution) {
+		
+		if(candidateSolution.length() != 81) {
+			return -2;
+		}
+		
+		if(verifyCorrectCharacters(candidateSolution) != 0) {
+			return -1;
 		}
 		
 		// returns 0 if the candidate solution is correct
