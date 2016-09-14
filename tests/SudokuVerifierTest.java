6caffa25e947be3d59286f07e4e7db26a6ab509f
somename <someemail>
2016-09-14 15:23:53 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,13 +8,14 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testCandidateContains_Zero() throws Exception {
+	public void testCandidateContains_Zero_1() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
 		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(verifier.verify(candidate), 1);
+		assertEq
+		uals(verifier.verify(candidate), 1);
 	}
 	@Test
-	public void testCandidateContains_Zero() throws Exception {
+	public void testCandidateContains_Zero_1() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
 		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertEquals(verifier.verify(candidate), 1);
