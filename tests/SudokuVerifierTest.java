20c2810e9b4656f325910be3203b7359c06c232a
somename <someemail>
2016-09-14 15:23:45 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -6,9 +6,15 @@ public class SudokuVerifierTest {
 
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
-	
+
 	@Test
-	public void testCandidateContains_Zero() {
+	public void testCandidateContains_Zero() throws Exception {
+		SudokuVerifier verifier = new SudokuVerifier();
+		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals(verifier.verify(candidate), 1);
+	}
+	@Test
+	public void testCandidateContains_Zero() throws Exception {
 		SudokuVerifier verifier = new SudokuVerifier();
 		String candidate = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		assertEquals(verifier.verify(candidate), 1);
