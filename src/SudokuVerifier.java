2c629997f4d642bcc534d8fd232c93ba9e3afac2
somename <someemail>
2016-09-14 15:52:42 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -10,24 +10,7 @@ public class SudokuVerifier {
 		
 		for (char c : candidateSolution.toCharArray())
 			if (c < '1' || c > '9') return 1;
-		
-		for (int gY = 0; gY < 3; gY++) {
-			for (int gX = 0; gX < 3; gX++) {
-				String reserved = "";
-				for (int sY = 0; sY < 3; sY++) {
-					for (int sX = 0; sX < 3; sX++) {
-						char c = candidateSolution.charAt(
-								(sY + gY * 3) * 9 + (sX + gX * 3));
-						
-						if (reserved.indexOf(c) != -1)
-							return 2;
-						
-						reserved += c;
-					}
-				}
-			}
-		}
-		
+
 		for (int y = 0; y < 9; y++) {
 			String reserved = "";
 			for (int x = 0; x < 9; x++) {
@@ -45,6 +28,23 @@ public class SudokuVerifier {
 				if (reserved.indexOf(c) != -1)
 					return 4;
 				reserved += c;
+			}
+		}
+		
+		for (int gY = 0; gY < 3; gY++) {
+			for (int gX = 0; gX < 3; gX++) {
+				String reserved = "";
+				for (int sY = 0; sY < 3; sY++) {
+					for (int sX = 0; sX < 3; sX++) {
+						char c = candidateSolution.charAt(
+								(sY + gY * 3) * 9 + (sX + gX * 3));
+						
+						if (reserved.indexOf(c) != -1)
+							return 2;
+						
+						reserved += c;
+					}
+				}
 			}
 		}
 		
