671fb09bc6019f24200b2a5f9e394a425905a87d
somename <someemail>
2016-09-14 15:19:32 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -2,6 +2,10 @@
 public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) {
+		
+		if(candidateSolution.length() != 81) {
+			return -1;
+		}
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -7,7 +7,7 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
-	private SudokuVerifier verifer;
+	private SudokuVerifier verifier;
 	
 	public void Setup() {
 		verifier = new SudokuVerifier();
@@ -20,17 +20,24 @@ public class SudokuVerifierTest {
 	
 	@Test
 	public void testVerify_withCorrectLength() {
-		
+		Setup();
+		int result = verifier.verify("41736982563215894795872431682543716979158643234691275828964357157329168416487529");
+		assertThat(result, is(0));
 	}
 	
 	@Test
 	public void testVerify_with80Chars() {
-		
+		Setup();
+		int result = verifier.verify("41736982563215894795872431682543716979158643234691275828964357157329168416487529");
+		assertThat(result, anyOf(is(-1), is(-2),is(-3),is(-4)));
 	}
 	
 	@Test
-	public void testVerify_with82Chars() {		
-
+	public void testVerify_with82Chars() {
+		Setup();
+		int result = verifier.verify("41736982563215894795872431682543716979158643234691275828964357157329168416487529");
+		assertThat(result, anyOf(is(-1), is(-2),is(-3),is(-4)));
 	}
 
+	
 }
