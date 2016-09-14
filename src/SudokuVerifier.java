aa18f3619620f8a5a40d6f65bc3a7bcd0cf5561e
somename <someemail>
2016-09-14 15:09:23 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -6,6 +6,16 @@ public class SudokuVerifier {
 	}
 	
 	public int verify(String candidateSolution) {
+		boolean negative = false;
+		char [] solution;
+		int i = 0;
+		while ((i < 81) && (!negative)) {
+			solution = candidateSolution.toCharArray();
+			if (solution[i] < 0) {
+				return -1;
+			}
+			i++;
+		}
 		// returns 0 if the candidate solution is correct
 		return 0;
 		
