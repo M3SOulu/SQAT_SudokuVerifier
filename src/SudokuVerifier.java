1dceeb1b30a6654eaf10b3f58186d02a566a20d9
somename <someemail>
2016-09-14 16:14:58 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -24,7 +24,7 @@ public class SudokuVerifier {
 	
 	public static int amountOfRows(String candidateSolution){
 		 String[] rows = splitStringToRows(candidateSolution);
-		if (rows.length == 10)
+		if (rows.length == 9)
 			return 0;
 		return -1;
 	}
