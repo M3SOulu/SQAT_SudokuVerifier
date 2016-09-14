2c1a8c7cd534b7480f26ca68b106ce6b810b6951
somename <someemail>
2016-09-14 15:34:27 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,11 +1,17 @@
 
 public class SudokuVerifier {
 	
+	I give up for this exercise for today. For me it needs
+	lots more time to actually figure out the way arrays
+	work in Java that is possible to achieve today.
+	
 	public int verify(String candidateSolution) {
 		
 		String sudokuString = candidateSolution;
 		
-		String testLength = sudokuString.length();
+		String sudokuArray[]= sudokuString.split("");
+		
+		
 		
 		
 		// returns 0 if the candidate solution is correct
