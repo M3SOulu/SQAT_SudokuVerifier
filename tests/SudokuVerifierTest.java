e9857241ebd273f4679bcdf2c433081be7219162
somename <someemail>
2016-09-14 15:22:23 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,7 +8,7 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 	
 	@Test
-	public void test
+	public void testCandidateContains_Zeros
 	//@Test
 	//public void testVerify() {
 	//	fail("Not yet implemented");
