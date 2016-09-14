022060a1635023c92ccaf4508203e09701bb1b72
somename <someemail>
2016-09-14 16:25:00 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -17,20 +17,23 @@ public class SudokuVerifier {
 	}
 	
 	public static int checkRow(String candidateSolution){
+		
+		/* Let's not get fancy yet, just make it work
 		int start = 0;
 		int stop = 8;
 		
 		for (int i = 0; i < 9; i++){
 		String sudokuRow = candidateSolution.substring(start, stop);
-			for (int j = start; j <= stop; j++){
-				
-				
+			for (int j = start; j <= stop; j++)7
 				break;
 			}
 		}
-		
+		*/
 		
 		String sudokuRow1 = candidateSolution.substring(0, 8);
+		for (int i = 0; i < 9; i++){
+			number1 
+		}
 		String sudokuRow2 = candidateSolution.substring(9, 17);
 		String sudokuRow3 = candidateSolution.substring(18, 26);
 		String sudokuRow4 = candidateSolution.substring(27, 35);
