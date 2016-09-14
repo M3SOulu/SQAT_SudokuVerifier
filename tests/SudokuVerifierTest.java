ffdeda989711ef1b61fc353d89f6586d27221f65
somename <someemail>
2016-09-14 15:57:27 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,12 +1,20 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class SudokuVerifierTest {
 
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
+	private SudokuVerifier verifier;
+	
+	@Before
+	public void setup() {
+		verifier = new SudokuVerifier();
+	}
+	
 	@Test
 	public void testVerify() {
 		fail("Not yet implemented");
@@ -15,14 +23,15 @@ public class SudokuVerifierTest {
 	@Test
 	public void rule2CorrectForCorrectSudoku() {
 		String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		SudokuVerifier verifier = new SudokuVerifier();
 		assertEquals("Rule 2 did not pass for correct sudoku",
 				verifier.NO_ERROR, verifier.verifySubGrid(correctSudoku));
 	}
 	
 	@Test
 	public void rule2CatchesError() {
-		String 
+		String incorrectSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		assertEquals("Subgrid error was not found when it exists",
+				verifier.SUBGRID_ERROR, verifier.verifySubGrid(incorrectSudoku));
 	}
 
 }
