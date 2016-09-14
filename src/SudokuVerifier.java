cffc947935c5b8de212757d55d93c7ba6091001a
somename <someemail>
2016-09-14 15:06:09 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,6 +1,10 @@
 
 public class SudokuVerifier {
 	
+	public static void main (String [] args) {
+		//Scanner sc = new Scanner (System.in)
+	}
+	
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
 		return 0;
