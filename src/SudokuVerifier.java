1348903d7efe154388b989182cc3395820de8135
somename <someemail>
2016-09-14 15:27:08 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -17,7 +17,7 @@ public class SudokuVerifier {
 				for (int sY = 0; sY < 3; sY++) {
 					for (int sX = 0; sX < 3; sX++) {
 						char c = candidateSolution.charAt(
-								(sY + gY * 3) * 3 + (sX + gY * 3));
+								(sY + gY * 3) * 3 + (sX + gX * 3));
 						
 						if (reserved.indexOf(c) != -1)
 							return 2;
@@ -29,7 +29,5 @@ public class SudokuVerifier {
 		}
 		
 		return 0;
-		
-		//check
 	}
 }
