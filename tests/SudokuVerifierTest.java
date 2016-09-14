e66c1dc1904a57e319417a8cd13580f42e4ad0d3
somename <someemail>
2016-09-14 15:27:02 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -11,14 +11,14 @@ public class SudokuVerifierTest {
 	public void testVerify_TooShortString() {
 		String testString = "123";
 		SudokuVerifier.verify(testString);
-		assertEquals("String is too short", 0,SudokuVerifier.verify(testString));
+		assertEquals("String is too short", -1,SudokuVerifier.verify(testString));
 	}
 	
 	@Test
 	public void testVerify_StringLengthIsEnough() {
 		String testString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		SudokuVerifier.verify(testString);
-		assertEquals("String lenght is incorrect", 1,SudokuVerifier.verify(testString));
+		assertEquals("String lenght is incorrect", -1,SudokuVerifier.verify(testString));
 	}
 	
 	@Test
@@ -32,7 +32,7 @@ public class SudokuVerifierTest {
 	public void testVerify_splitOneRow() {
 		String testString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		SudokuVerifier.splitStringToRows(testString);
-		assertEquals("Row is not correct length", 1,SudokuVerifier.verify(testString));
+		assertEquals("Row is not correct length", 0,SudokuVerifier.verify(testString));
 	}
 
 
