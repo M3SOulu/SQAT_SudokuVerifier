a79089b3e560427c47d1e94267e5a9610bc17346
somename <someemail>
2016-09-14 16:28:11 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,7 +8,7 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify_TooShortString() {
+	public void testVerify_ToShortString() {
 		String testString = "123";
 		SudokuVerifier.verify(testString);
 		assertEquals("String is too short", -1,SudokuVerifier.verify(testString));
