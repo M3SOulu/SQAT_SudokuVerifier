54ad87172cd5f055792d92178342089bd4e15553
somename <someemail>
2016-09-14 15:17:54 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -13,11 +13,11 @@ public class SudokuVerifier {
 		
 		for (int gY = 0; gY < 3; gY++) {
 			for (int gX = 0; gX < 3; gX++) {
-				ArrayList<Integer> reserved = new ArrayList<Integer>();
+				String reserved;
 				for (int sY = 0; sY < 3; sY++) {
 					for (int sX = 0; sX < 3; sX++) {
-						if (reserved.contains(candidateSolution[
-								(sY + gY * 3) * 3 + (sX + gY * 3)]))
+						if (reserved.contains(candidateSolution.charAt(
+								(sY + gY * 3) * 3 + (sX + gY * 3)))
 							return 2;
 					}
 				}
