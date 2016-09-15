91dd1b67e8e377083d6b7d497f3498d141e2cc1c
somename <someemail>
2016-09-15 15:52:01 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -57,7 +57,7 @@ public class SudokuVerifier {
 		for(int i=0; i<str.length(); i++){ // Check for each charter if exist in string.
 			for(int j=i+1; j<str.length(); j++){
 				if(str.charAt(i) == str.charAt(j))
-					return false;
+					return true;
 			}
 		}
 		return false;
