02e1d48b34894dd7d6335fafb0230c3120a53c6c
somename <someemail>
2016-09-15 21:11:06 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -67,7 +67,15 @@ public class SudokuVerifierTest {
 	
 	@Test
 	public void rule4CatchesError() {
-		String incorrectSudoku = "123456789912345678891234567789123456678912345567891234456782123345678912234567891";
+		String incorrectSudoku = "123456789"
+							   + "912345678"
+							   + "891234567"
+							   + "789123456"
+							   + "678912345"
+							   + "567891234"
+							   + "456782123"
+							   + "345678912"
+							   + "234567891";
 		assertEquals("Erratic global column was not found",
 				verifier.GLOBAL_COLUMN_ERROR, verifier.verifyGlobalColumn(incorrectSudoku));
 	}
