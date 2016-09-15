9f3b9c643786b70e48a3563c4d3bd097a7202cd4
somename <someemail>
2016-09-15 15:24:24 +0300

    besouro automatic message

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		SudokuVerifier sudo = new SudokuVerifier();
 		String test = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		sudo.isDigitsOnly(str)
+		System.out.println("Result is " + sudo.isDigitsOnly(test));
 	}
 
 }
