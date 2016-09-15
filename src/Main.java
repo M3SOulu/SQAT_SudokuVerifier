9be9b2aa72b62a682011607915ce5cdfa06b05a4
somename <someemail>
2016-09-15 15:24:02 +0300

    besouro automatic message

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SudokuVerifier sudo = new SudokuVerifier();
+		String test = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		sudo.isDigitsOnly(str)
+	}
+
+}
