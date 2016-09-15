9cbdd4a0d89059c8ab5cee28402034f2a361fb49
somename <someemail>
2016-09-15 16:09:42 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -172,7 +172,7 @@ public class SudokuVerifierTest {
 	public void testNoRepeatInColumn(){
 		SudokuVerifier sudoku = new SudokuVerifier();
 		
-		String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		String test = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 
 		assertEquals(expected, test);
 	}
