4138b8e3605d81bf55c60d7d2302cc14fc8ee957
somename <someemail>
2016-09-15 16:03:22 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -106,7 +106,7 @@ public class SudokuVerifierTest {
 	}
 
 	@Test
-	public void testNoRepeatInSubGridsWithFalse(){
+	public void testNoRepeatInSubGridsWithInvalid(){
 		SudokuVerifier sudoku = new SudokuVerifier();
 		
 		String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
@@ -134,6 +134,15 @@ public class SudokuVerifierTest {
 
 	@Test
 	public void testNoRepeatInRowsWithCorrect(){
+		SudokuVerifier sudoku = new SudokuVerifier();
+		
+		String test = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		assertTrue(sudoku.noRepeatInRows(test));
+	}
+
+	@Test
+	public void testNoRepeatInRowsWithInvalid(){
 		SudokuVerifier sudoku = new SudokuVerifier();
 		
 		String test = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
