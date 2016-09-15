0087cf17c2cb969ce6dc1e439e87109a74cb8c75
somename <someemail>
2016-09-15 15:45:51 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -72,7 +72,7 @@ public class SudokuVerifierTest {
 		
 		String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 		
-		String grid = sudoku.generateGrid(0, test);
+		String grid = sudoku.generateGrid(5, test);
 		String expected = "123912891";
 		
 		assertEquals("The grid is not as expected", expected, grid);
