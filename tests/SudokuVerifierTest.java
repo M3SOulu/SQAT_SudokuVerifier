71176601ec595e8dc6ed15bb8b788e7d8e4524d6
somename <someemail>
2016-09-15 16:00:17 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -120,7 +120,7 @@ public class SudokuVerifierTest {
 		
 		String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 		
-		assertEquals(sudoku.generateRow(0, test), "123456789");
+		assertEquals("123456789",sudoku.generateRow(0, test));
 	}
 
 	@Test
