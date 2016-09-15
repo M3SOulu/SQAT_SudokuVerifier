4df051ce96da66e2ae27c19d9cb7f7ca0e1380d5
somename <someemail>
2016-09-15 15:45:17 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -55,9 +55,15 @@ public class SudokuVerifierTest {
 	}
 	
 	@Test
-	public void testGenerateGrid(){
+	public void testGenerateGrid0(){
 		SudokuVerifier sudoku = new SudokuVerifier();
 		
 		String test = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		
+		String grid = sudoku.generateGrid(0, test);
+		String expected = "123912891";
+		
+		assertEquals("The grid is not as expected", expected, grid);
+		
 	}
 }
