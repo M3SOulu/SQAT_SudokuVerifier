3c14aef228e442f8bc8ea5840c8ed8c57b62d37a
somename <someemail>
2016-09-15 15:06:08 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -10,16 +10,16 @@ public class SudokuVerifier {
 			return -3;
 		else if(!noRepeatInColumns(str))
 			return -4;
-		else		// returns 0 if the candidate solution is correct
+		else		// returns 0 if the candidate solution is correct.
 			return 0;
 	}
 	private boolean isDigitsOnly(String str) {
 		if(str.length() != 81) // Check length
 			return false;
 		
-		for (int i=0; i<81; i++){ // Check every digit as char
+		for (int i=0; i<81; i++){ // Check every digit as char.
 			char digit = str.charAt(i);
-			if(!(0<=digit && digit<=9)) // If not between 0 and 9 return false
+			if(!(0<=digit && digit<=9)) // If not between 0 and 9 return false.
 				return false;
 		}
 		return false;
