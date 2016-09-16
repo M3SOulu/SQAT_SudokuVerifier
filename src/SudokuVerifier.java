7ab30a74ccebc8b98a1c819025a4a736ce5988d4
somename <someemail>
2016-09-16 14:59:51 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,28 +1,55 @@
 import java.util.*;
 
 public class SudokuVerifier {
+	private int[][] sudoku;
 	
 	public int verify(String candidateSolution) {
 		String verifiable = candidateSolution;
+		sudoku = new int [9][9];
 		
 		//Check R1
-		if(!(verifiable.matches("[0-9]+") && verifiable.length() == 81)) {
+		if(checkIfStringValid(verifiable) == -1) {
 			return -1;
 		}
+		//Read into multidimensional array
+		readIntoArray(verifiable);
 		
-		//Check R3
-		int helper = 0;
-		for(int i = 0; i < 81; i=+8) {
-			String subset = verifiable.substring(i, i+8);
-			for(int j = 0; i < 9; i++) {
-				if(i != subset.charAt(helper) && subset.contains(subset.substring(helper, helper))) {
-					return -3;
+		//Iterate through
+		for(int i = 0; i < sudoku.length; i++) {
+			for(int j = 0; j < sudoku.length; j++) {
+				int sudokuCell = sudoku[i][j]; 
+				//Check rows (R3)
+				for(int x = 0; x < sudoku.length; x++) {
+					if((j != x) && (sudokuCell == sudoku[i][j])) {
+						return -3;
+					}
 				}
-				helper++;
 			}
 		}
 
 		return 0;
 		
+		}
+		
+	
+	public int[][] getSudoku() {
+		return this.sudoku;
+	}
+	
+	private int checkIfStringValid(String sudoku) {
+		if(!(sudoku.matches("[0-9]+") && sudoku.length() == 81)) {
+			return -1;
+		}
+		return 1;
+	}
+		
+	private void readIntoArray(String verifiable) {
+		int helper = 0;
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				sudoku[i][j] = Character.getNumericValue(verifiable.charAt(helper));
+				helper++;
+			}
+		}
 	}
 }
